a075c96b2014fc7a7a69c9523f15ca0451b8fdb8
Casey Kurosawa <caseykurosawa@datawire.io>
2024-04-24 13:59:49 -0700

    arrays slice refactor

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,12 +3,24 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{4,5,6,7,8}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{4,5,6,7,8}
 
-	got := Sum(numbers)
-	want := 30
+		got := Sum(numbers)
+		want := 30
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+	t.Run("any size collection", func(t *testing.T) {
+		numbers := []int{4,5,6}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
