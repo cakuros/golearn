27a2a17d9336c0d41c550b7c86ab0b97b1937565
Casey Kurosawa <caseykurosawa@datawire.io>
2024-04-24 11:33:09 -0700

    hello const refactor succeed

--- src/hello.go
+++ src/hello.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	
+	return englishHelloPrefix + name
 }
 
 func main() {
