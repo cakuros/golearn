b4694f49c4609a2f4bf1d6780f0a52bcadd59c36
Casey Kurosawa <caseykurosawa@datawire.io>
2024-04-24 11:51:31 -0700

    hello spanish support succeed

--- src/hello_test.go
+++ src/hello_test.go
@@ -4,15 +4,18 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
-
 		assertCorrectMessage(got, want, t)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
-
+		assertCorrectMessage(got, want, t)
+	})
+	t.Run("say hello in Spanish", func(t *testing.T) {
+		got := Hello("Rosie", "Spanish")
+		want := "Hola, Rosie"
 		assertCorrectMessage(got, want, t)
 	})
 }
