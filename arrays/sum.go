595c4a0c6a26d88da82c5201f69a3ccbc6c83aa5
Casey Kurosawa <caseykurosawa@datawire.io>
2024-04-25 12:45:44 -0700

    summalltails function

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,11 +9,25 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lenOfArgs := len (numbersToSum)
-	sums := make([]int, lenOfArgs)
+	var sums []int
 
-	for index, numbers := range numbersToSum {
-		sums[index] = Sum(numbers)
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
