52e855f4fccb02b451e8c1f6daa921184b3a00c7
Casey Kurosawa <caseykurosawa@datawire.io>
2024-04-26 09:55:49 -0700

    perimeter func

---
+++ structs/shapes_test.go
@@ -0,0 +1,14 @@
+package shapes
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	t.Run("Test Perimeter", func(t *testing.T) {
+		got := Perimiter(10.0, 10.0)
+		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+}
